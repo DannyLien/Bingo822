a3fe70d82ffe2182a7f0f4f87cf3983c18d0b7dd
DannyLien <danny.lien1109@gmail.com>
2025-01-26 12:07:50 +0800

    1. Gradle, API35;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.hank.bingo822"
-    compileSdk = 34
+    compileSdk = 35
+
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.hank.bingo822"
@@ -36,7 +42,31 @@ android {
 }
 
 dependencies {
-
+    // firebase : auth , realtime , ui
+    // https://firebase.google.com/docs/android/setup?hl=zh-tw
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation ("com.firebaseui:firebase-ui-auth:8.0.2")//7.2.0
+    implementation("com.google.gms:google-services:4.4.2")
+    // Coil
+    implementation("io.coil-kt:coil:2.7.0")
+    // Gson , com.squareup.okhttp3 , okhttp , Maven Central
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+    // Room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+    // lifecycle
+    val lifecycle_version = "2.8.4"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Annotation processor
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+    //init
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -45,3 +75,19 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
